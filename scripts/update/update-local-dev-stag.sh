547c466f8a104509fad1ecd7a0dfcc54e9979b1b
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-12-24 23:10:37 +0200

    chore: renamed wp-update.sh

--- scripts/update/update-local-dev-stag.sh
+++ scripts/update/update-local-dev-stag.sh
@@ -4,13 +4,14 @@
 
 git checkout dev
 git pull
-# TODO update version in Dockerfiles
+# TODO check latest version with docker and update version in Dockerfiles
 docker-compose pull wordpress-template-wordpress
 docker-compose pull wordpress-template-database
 docker-compose up --detach
-sleep 120
+# TODO poll until some configured text is found on landing page + timeout
 docker exec -it wordpress-template-wordpress \
   su -c '/opt/bitnami/wp-cli/bin/wp plugin update --all --debug' bitnami
+# TODO check that configured text is still shown on landing page + timeout
 docker-compose stop
 date >> UPDATELOG
 git add .
