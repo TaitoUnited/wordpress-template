95e33a4110e7e4e78272db4cb4182e71452382a3
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-01-24 16:44:04 +0200

    chore: update taito configs

--- scripts/taito-template/init.sh
+++ scripts/taito-template/init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
+# TODO: most of this has been pretty much copy pasted from server-template
 : "${taito_organization:?}"
 : "${taito_company:?}"
-: "${taito_repo_name:?}"
-: "${taito_repo_name_alt:?}"
+: "${taito_vc_repository:?}"
+: "${taito_vc_repository_alt:?}"
 
 : "${template_default_taito_image:?}"
 : "${template_default_organization:?}"
+: "${template_default_organization_abbr:?}"
+: "${template_default_github_organization:?}"
+: "${template_default_sentry_organization:?}"
 : "${template_default_domain:?}"
 : "${template_default_domain_prod:?}"
 : "${template_default_zone:?}"
@@ -27,6 +30,13 @@
 : "${template_project_path:?}"
 : "${mode:?}"
 
+# Determine sed options
+if [ "$(uname)" = "Darwin" ]; then
+ sedi="-i ''"
+else
+ sedi="-i"
+fi
+
 # Remove .gitignore to allow committing data to git
 rm wordpress/data/.gitignore
 mv wordpress/data/.gitkeep wordpress/data/.gitignore
@@ -36,19 +46,19 @@ mv wordpress/data/.gitkeep wordpress/data/.gitignore
 rm LICENSE
 
 # Replace repository url in package.json
-sed -i -- \
-  "s|TaitoUnited/wordpress-template.git|${taito_organization}/${taito_repo_name}.git|g" package.json
+sed ${sedi} -- \
+  "s|TaitoUnited/wordpress-template.git|${taito_organization}/${taito_vc_repository}.git|g" package.json
 
 # Replace default admin password
 admin_password=$(openssl rand -base64 40 | sed -e 's/[^a-zA-Z0-9]//g')
 if [[ ${#admin_password} -gt 30 ]]; then
   admin_password="${admin_password: -30}"
 fi
-sed -i -- \
+sed ${sedi} -- \
   "s|admin-pass-change-it-7983p4nWgRE2p4No2d9|${admin_password}|g" scripts/helm/values.yaml
-sed -i -- \
+sed ${sedi} -- \
   "s|admin-pass-change-it-7983p4nWgRE2p4No2d9|${admin_password}|g" docker-compose.yaml
-sed -i -- \
+sed ${sedi} -- \
   "s|admin-pass-change-it-7983p4nWgRE2p4No2d9|${admin_password}|g" README.md
 
 # Add some do not modify notes
@@ -105,83 +115,67 @@ fi
 
 echo "Replacing project and company names in files. Please wait..."
 
-find . -type f -exec sed -i \
+find . -type f -exec sed ${sedi} \
   -e "s/wptemplate/${taito_project_short}/g" 2> /dev/null {} \;
-find . -type f -exec sed -i \
-  -e "s/wordpress_template/${taito_repo_name_alt}/g" 2> /dev/null {} \;
-find . -type f -exec sed -i \
-  -e "s/wordpress-template/${taito_repo_name}/g" 2> /dev/null {} \;
-find . -type f -exec sed -i \
+find . -type f -exec sed ${sedi} \
+  -e "s/wordpress_template/${taito_vc_repository_alt}/g" 2> /dev/null {} \;
+find . -type f -exec sed ${sedi} \
+  -e "s/wordpress-template/${taito_vc_repository}/g" 2> /dev/null {} \;
+find . -type f -exec sed ${sedi} \
   -e "s/companyname/${taito_company}/g" 2> /dev/null {} \;
-find . -type f -exec sed -i \
+find . -type f -exec sed ${sedi} \
   -e "s/WORDPRESS-TEMPLATE/wordpress-template/g" 2> /dev/null {} \;
+
+# TODO: everything below has been pretty much copy pasted from server-template
 
 echo "Generating unique random ports (avoid conflicts with other projects)..."
 
 ingress_port=$(shuf -i 8000-9999 -n 1)
 db_port=$(shuf -i 6000-7999 -n 1)
-sed -i -- "s/7587/${db_port}/g" taito-config.sh &> /dev/null
-sed -i -- "s/7587/${db_port}/g" docker-compose.yaml &> /dev/null
-sed -i -- "s/4635/${ingress_port}/g" docker-compose.yaml taito-config.sh \
+sed ${sedi} -- "s/7587/${db_port}/g" taito-config.sh &> /dev/null
+sed ${sedi} -- "s/7587/${db_port}/g" docker-compose.yaml &> /dev/null
+sed ${sedi} -- "s/4635/${ingress_port}/g" docker-compose.yaml taito-config.sh \
   ./admin/package.json ./client/package.json &> /dev/null
 
 echo "Replacing template variables with the given settings..."
 
-sed -i -- \
-  "s/export taito_company=\".*\"/export taito_company=\"${taito_company}\"/g" taito-config.sh
-sed -i -- \
-  "s/export taito_family=\".*\"/export taito_family=\"${taito_family:-}\"/g" taito-config.sh
-sed -i -- \
-  "s/export taito_application=\".*\"/export taito_application=\"${taito_application:-}\"/g" taito-config.sh
-sed -i -- \
-  "s/export taito_suffix=\".*\"/export taito_suffix=\"${taito_suffix:-}\"/g" taito-config.sh
-sed -i -- \
-  "s/export taito_repo_name=\".*\"/export taito_repo_name=\"${taito_repo_name}\"/g" taito-config.sh
-sed -i -- \
-  "s/export taito_project=\".*\"/export taito_project=\"${taito_repo_name}\"/g" taito-config.sh
+# Replace template variables in taito-config.sh with the given settings
+sed ${sedi} -- "s/taito_company=.*/taito_company=${taito_company}/g" taito-config.sh
+sed ${sedi} -- "s/taito_family=.*/taito_family=${taito_family:-}/g" taito-config.sh
+sed ${sedi} -- "s/taito_application=.*/taito_application=${taito_application:-}/g" taito-config.sh
+sed ${sedi} -- "s/taito_suffix=.*/taito_suffix=${taito_suffix:-}/g" taito-config.sh
+sed ${sedi} -- "s/taito_project=.*/taito_project=${taito_vc_repository}/g" taito-config.sh
 
 echo "Replacing template variables with the user specific settings..."
 
-sed -i -- \
-  "s/\${template_default_organization:?}/${template_default_organization}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_domain:?}/${template_default_domain}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_domain_prod:?}/${template_default_domain_prod}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_zone:?}/${template_default_zone}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_zone_prod:?}/${template_default_zone_prod}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_provider:?}/${template_default_provider}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_provider_org_id:?}/${template_default_provider_org_id}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_provider_org_id_prod:?}/${template_default_provider_org_id_prod}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_provider_region:?}/${template_default_provider_region}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_provider_zone:?}/${template_default_provider_zone}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_provider_region_prod:?}/${template_default_provider_region_prod}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_provider_zone_prod:?}/${template_default_provider_zone_prod}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_registry:?}/${template_default_registry}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_source_git:?}/${template_default_source_git}/g" taito-config.sh
-sed -i -- \
-  "s/\${template_default_dest_git:?}/${template_default_dest_git}/g" taito-config.sh
-sed -i -- \
+# Replace template variables in taito-config.sh with user specific settings
+sed ${sedi} -- "s/\${template_default_organization:?}/${template_default_organization}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_organization_abbr:?}/${template_default_organization_abbr}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_github_organization:?}/${template_default_github_organization}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_sentry_organization:?}/${template_default_sentry_organization}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_domain:?}/${template_default_domain}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_domain_prod:?}/${template_default_domain_prod}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_zone:?}/${template_default_zone}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_zone_prod:?}/${template_default_zone_prod}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_provider:?}/${template_default_provider}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_provider_org_id:?}/${template_default_provider_org_id}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_provider_org_id_prod:?}/${template_default_provider_org_id_prod}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_provider_region:?}/${template_default_provider_region}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_provider_zone:?}/${template_default_provider_zone}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_provider_region_prod:?}/${template_default_provider_region_prod}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_provider_zone_prod:?}/${template_default_provider_zone_prod}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_registry:?}/${template_default_registry}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_source_git:?}/${template_default_source_git}/g" taito-config.sh
+sed ${sedi} -- "s/\${template_default_dest_git:?}/${template_default_dest_git}/g" taito-config.sh
+sed ${sedi} -- \
   "s/\${template_default_kubernetes:?}/${template_default_kubernetes}/g" taito-config.sh
-sed -i -- \
+sed ${sedi} -- \
   "s/\${template_default_mysql:?}/${template_default_mysql}/g" taito-config.sh
 
 echo "Removing template settings from cloudbuild.yaml..."
 
-sed -i -- \
-  "s|\${_TEMPLATE_DEFAULT_TAITO_IMAGE}|${template_default_taito_image}|g" cloudbuild.yaml
-sed -i -- '/_TEMPLATE_DEFAULT_/d' cloudbuild.yaml
-sed -i -- '/template_default_taito_image/d' cloudbuild.yaml
+sed ${sedi} -- "s|\${_TEMPLATE_DEFAULT_TAITO_IMAGE}|${template_default_taito_image}|g" cloudbuild.yaml
+sed ${sedi} -- '/_TEMPLATE_DEFAULT_/d' cloudbuild.yaml
+sed ${sedi} -- '/template_default_taito_image/d' cloudbuild.yaml
 
 rm -f temp
--- taito-config.sh
+++ taito-config.sh
@@ -1,177 +1,203 @@
 #!/bin/bash
+# shellcheck disable=SC2034
+: "${taito_env:?}"
+: "${taito_target_env:?}"
+
+# Configuration instructions:
+# - https://github.com/TaitoUnited/taito-cli/blob/master/docs/manual/04-configuration.md
+# - https://github.com/TaitoUnited/taito-cli/blob/master/docs/plugins.md
 
 # Taito-cli
-export taito_version="1"
-export taito_image="taitounited/taito-cli:latest"
-export taito_extensions=""
-export taito_plugins=" \
-  mysql-db \
-  docker \
-  docker-compose:local \
-  terraform:-local secrets:-local kube-secrets:-local \
-  kubectl:-local helm:-local \
-  gcloud:-local gcloud-builder:-local \
-  semantic npm git links-global \
+taito_version=1
+taito_plugins="
+  mysql-db
+  docker
+  docker-compose:local
+  terraform:-local secrets:-local kube-secrets:-local
+  kubectl:-local helm:-local
+  gcloud:-local gcloud-builder:-local
+  semantic npm git links-global
 "
 
-# Project
-export taito_organization="${template_default_organization:?}"
-export taito_project="wordpress-template"
-export taito_project_short="wptemplate" # Max 10 characters
-export taito_company="companyname"
-export taito_family=""
-export taito_application="template"
-export taito_suffix=""
+# Project labeling
+taito_organization=${template_default_organization:?}
+taito_organization_abbr=${template_default_organization_abbr:?}
+taito_project=wordpress-template
+# taito_project_short max 10 characters
+taito_project_short=wptemplate
+taito_company=companyname
+taito_family=
+taito_application=template
+taito_suffix=
 
-# Provider and namespaces
-export taito_zone="${template_default_zone:?}"
-export taito_provider="${template_default_provider:?}"
-export taito_provider_region="${template_default_provider_region:?}"
-export taito_provider_zone="${template_default_provider_zone:?}"
-export taito_namespace="${taito_project}-${taito_env:?}"
-export taito_resource_namespace="${taito_company}-prod"
-export taito_environments="dev stag prod"
+# Assets
+taito_project_icon=$taito_project-dev.${template_default_domain:?}/favicon.ico
 
-# Repositories
-# TODO change taito_repo and taito_registry naming, add also repo url?
-export taito_repo_location="github-${taito_organization}"
-export taito_repo_name="${taito_project}"
-export taito_registry="${template_default_registry:?}/${taito_zone}/${taito_repo_location}-${taito_repo_name}"
-
-# Stack
-export taito_targets="wordpress database"
-export taito_databases="database"
-export taito_storages="${taito_project}-${taito_env}"
-export taito_networks="default"
-
-# Database definitions for database plugins
-export db_database_instance="${template_default_mysql:?}"
-export db_database_type="mysql"
-export db_database_name="${taito_project_short}${taito_env}"
-export db_database_host="127.0.0.1"
-export db_database_proxy_port="5001"
-export db_database_port="${db_database_proxy_port}"
+# Environments
+taito_environments="dev stag prod"
+taito_env=${taito_env/canary/prod} # canary -> prod
 
 # URLs
-export taito_domain="${taito_project}-${taito_env:?}.${template_default_domain:?}"
-export taito_app_url="https://${taito_domain}"
+taito_domain=$taito_project-$taito_target_env.template_default_domain:?}
+taito_app_url=https://$taito_domain
+taito_static_url=
 
-# Wordpress configs
-# WARNING: Setting this from true to false deletes the existing persistent disk
-export wordpress_persistence_enabled="false"
-export wordpress_plugin_update_flags="--all --debug --minor"
+# Provider and namespaces
+taito_provider=${template_default_provider:?}
+taito_provider_region=${template_default_provider_region:?}
+taito_provider_zone=${template_default_provider_zone:?}
+taito_zone=${template_default_zone:?}
+taito_namespace=$taito_project-$taito_env
+taito_resource_namespace=$taito_organization_abbr-$taito_company-dev
 
-# Docker plugin
-export dockerfile=Dockerfile
+# Repositories
+taito_vc_repository=$taito_project
+taito_image_registry=${template_default_registry:?}/$taito_zone/$taito_vc_repository
 
-# Google Cloud plugin
-export gcloud_org_id="${template_default_provider_org_id:?}"
-export gcloud_sql_proxy_port="${db_database_proxy_port}"
-export gcloud_cdn_enabled=false
+# Stack
+taito_targets="wordpress database"
+taito_databases="database"
+taito_storages="$taito_project-$taito_env"
+taito_networks="default"
 
-# Kubernetes plugin
-export kubectl_name="${template_default_kubernetes:?}"
-export kubectl_replicas="1"
+# Database definitions for database plugins
+db_database_instance=${template_default_mysql:?}
+db_database_type=mysql
+db_database_name=${taito_project_short}${taito_env}
+db_database_host=127.0.0.1
+db_database_proxy_port=5001
+db_database_port=$db_database_proxy_port
 
-# Helm plugin
-# export helm_deploy_options="--recreate-pods" # Force restart
+# Messaging
+taito_messaging_app=slack
+taito_messaging_channel=companyname
+taito_messaging_builds_channel=builds
+taito_messaging_monitoring_channel=monitoring
+taito_messaging_webhook=
 
-# Template plugin
-export template_name="WORDPRESS-TEMPLATE"
-export template_source_git="git@github.com:TaitoUnited"
+# Monitoring
+taito_monitoring_paths="/"
 
 # CI/CD settings
 # NOTE: Most of these should be enabled for dev and feature envs only
-export ci_exec_build=false        # build a container if does not exist already
-export ci_exec_deploy=true        # deploy automatically
-export ci_exec_test=false         # execute test suites after deploy
-export ci_exec_test_wait=1        # how many seconds to wait for deployment/restart
-export ci_exec_test_init=false    # run 'init --clean' before each test suite
-export ci_exec_revert=false       # revert deploy automatically on fail
+ci_exec_build=false        # build a container if does not exist already
+ci_exec_deploy=true        # deploy automatically
+ci_exec_test=false         # execute test suites after deploy
+ci_exec_test_wait=1        # how many seconds to wait for deployment/restart
+ci_exec_test_init=false    # run 'init --clean' before each test suite
+ci_exec_revert=false       # revert deploy automatically on fail
+
+# ------ Plugin specific settings ------
+
+# Hour reporting and issue management plugins
+toggl_project_id=
+toggl_tasks="" # For example "task:12345 another-task:67890"
+jira_project_id=
+
+# Template plugin
+template_name=WORDPRESS-TEMPLATE
+template_source_git=git@github.com:TaitoUnited
+
+# Google Cloud plugin
+gcloud_org_id=${template_default_provider_org_id:?}
+gcloud_sql_proxy_port=$db_database_proxy_port
+
+# Kubernetes plugin
+kubectl_name=${template_default_kubernetes:?}
+kubectl_replicas=1
+
+# Helm plugin
+# helm_deploy_options="--recreate-pods" # Force restart
+
+# Wordpress settings
+# WARNING: Setting this from true to false deletes the existing persistent disk
+wordpress_persistence_enabled=false
+wordpress_plugin_update_flags="--all --debug --minor"
 
 # --- Override settings for different environments ---
 
-case "${taito_env}" in
+case $taito_env in
   prod)
-    export taito_app_url="https://${taito_namespace}.${template_default_domain:?}"
-    export taito_zone="${template_default_zone_prod:?}"
-    export taito_provider_region="${template_default_provider_region_prod:?}"
-    export taito_provider_zone="${template_default_provider_zone_prod:?}"
-    export taito_resource_namespace="${taito_company}-prod"
-    export gcloud_org_id="${template_default_provider_org_id_prod:?}"
+    taito_zone=${template_default_zone_prod:?}
+    taito_provider_region=${template_default_provider_region_prod:?}
+    taito_provider_zone=${template_default_provider_zone_prod:?}
+    taito_resource_namespace=$taito_organization_abbr-$taito_company-prod
+    gcloud_org_id=${template_default_provider_org_id_prod:?}
 
     # NOTE: Set production domain here
-    export taito_domain="${taito_project}-${taito_env:?}.${template_default_domain_prod:?}"
-    export taito_app_url="https://${taito_domain}"
-    export kubectl_replicas="1"
+    taito_domain=$taito_project-$taito_target_env.${template_default_domain_prod:?}
+    taito_app_url=https://$taito_domain
+    kubectl_replicas=1
+    monitoring_enabled=true
+
+    # Settings for canary
+    if [[ $taito_target_env == "canary" ]]; then
+      taito_domain=$taito_project-$taito_target_env.${template_default_domain_prod:?}
+    fi
     ;;
   stag)
-    export taito_app_url="https://${taito_namespace}.${template_default_domain:?}"
-    export taito_zone="${template_default_zone_prod:?}"
-    export taito_provider_region="${template_default_provider_region_prod:?}"
-    export taito_provider_zone="${template_default_provider_zone_prod:?}"
-    export taito_resource_namespace="${taito_company}-prod"
-    export gcloud_org_id="${template_default_provider_org_id_prod:?}"
+    taito_zone=${template_default_zone_prod:?}
+    taito_provider_region=${template_default_provider_region_prod:?}
+    taito_provider_zone=${template_default_provider_zone_prod:?}
+    taito_resource_namespace=$taito_organization_abbr-$taito_company-prod
+    gcloud_org_id=${template_default_provider_org_id_prod:?}
 
-    export taito_domain="${taito_project}-${taito_env:?}.${template_default_domain_prod:?}"
-    export taito_app_url="https://${taito_domain}"
+    taito_domain=$taito_project-$taito_env.${template_default_domain_prod:?}
+    taito_app_url=https://$taito_domain
 
     # NOTE: dev/test not deployed on Kubernetes, therefore containers are
     # built for staging.
-    export ci_exec_build=true        # allow build of a new container
-    export ci_exec_deploy=true       # deploy automatically
+    ci_exec_build=true        # allow build of a new container
+    ci_exec_deploy=true       # deploy automatically
     ;;
   local)
     # local overrides
-    export ci_exec_test_init=false   # run 'init --clean' before each test suite
-    export taito_app_url="http://localhost:4635"
-    export db_database_external_port="7587"
-    export db_database_host="${taito_project}-database"
-    export db_database_port="3306"
-    export db_database_password="secret"
-    export db_database_username="${taito_project_short}${taito_env}"
+    ci_exec_test_init=false   # run 'init --clean' before each test suite
+    taito_app_url=http://localhost:4635
+    db_database_external_port=7587
+    db_database_host=$taito_project-database
+    db_database_port=3306
+    db_database_password=secret
+    db_database_username=${taito_project_short}${taito_env}
 esac
 
 # --- Derived values ---
 
-# Namespaces
-export taito_resource_namespace_id="${taito_organization}-${taito_resource_namespace}"
-
 # URLs
-export taito_admin_url="${taito_app_url}/wp-admin/"
+taito_admin_url=$taito_app_url/wp-admin/
+
+# Provider and namespaces
+taito_resource_namespace_id=$taito_resource_namespace
 
 # Google Cloud plugin
-export gcloud_region="${taito_provider_region}"
-export gcloud_zone="${taito_provider_zone}"
-export gcloud_project="${taito_zone}"
-export gcloud_storage_locations="EU"
-export gcloud_storage_classes="MULTI_REGIONAL"
+gcloud_region=$taito_provider_region
+gcloud_zone=$taito_provider_zone
+gcloud_project=$taito_zone
+gcloud_storage_locations=EU
+gcloud_storage_classes=MULTI_REGIONAL
 
 # Kubernetes plugin
-export kubectl_cluster="gke_${taito_zone}_${gcloud_zone}_${kubectl_name}"
-export kubectl_user="${kubectl_cluster}"
+kubectl_cluster=gke_${taito_zone}_${gcloud_zone}_${kubectl_name}
+kubectl_user=$kubectl_cluster
 
 # Link plugin
-export link_urls="\
-  * app[:ENV]#app=${taito_app_url} Application (:ENV) \
-  * admin[:ENV]#admin=${taito_admin_url} Admin user interface (:ENV) \
-  * docs=https://github.com/${taito_organization}/${taito_repo_name}/wiki Project documentation \
-  * git=https://github.com/${taito_organization}/${taito_repo_name} GitHub repository \
-  * kanban=https://github.com/${taito_organization}/${taito_repo_name}/projects Kanban boards \
-  * project[:ENV]=https://console.cloud.google.com/home/dashboard?project=${taito_resource_namespace_id} Google project (:ENV) \
-  * resources[:ENV]=https://console.cloud.google.com/home/dashboard?project=${taito_resource_namespace_id} Google resources (:ENV) \
-  * services[:ENV]=https://console.cloud.google.com/apis/credentials?project=${taito_resource_namespace_id} Google services (:ENV) \
-  * builds=https://console.cloud.google.com/gcr/builds?project=${taito_zone}&query=source.repo_source.repo_name%3D%22${taito_repo_location}-${taito_repo_name}%22 Build logs \
-  * storage:ENV#storage=https://console.cloud.google.com/storage/browser/${taito_project}-${taito_env}?project=${taito_resource_namespace_id} Storage bucket (:ENV) \
-  * logs:ENV#logs=https://console.cloud.google.com/logs/viewer?project=${taito_zone}&minLogLevel=0&expandAll=false&resource=container%2Fcluster_name%2F${kubectl_name}%2Fnamespace_id%2F${taito_namespace} Logs (:ENV) \
-  * uptime=https://app.google.stackdriver.com/uptime?project=${taito_zone} Uptime monitoring (Stackdriver) \
+link_urls="
+  * app[:ENV]#app=$taito_app_url Application (:ENV)
+  * admin[:ENV]#admin=$taito_admin_url Admin user interface (:ENV)
+  * docs=https://github.com/${template_default_github_organization:?}/$taito_vc_repository/wiki Project documentation
+  * git=https://github.com/${template_default_github_organization:?}/$taito_vc_repository GitHub repository
+  * kanban=https://github.com/${template_default_github_organization:?}/$taito_vc_repository/projects Kanban boards
+  * services[:ENV]=https://console.cloud.google.com/apis/credentials?project=$taito_resource_namespace_id Google services (:ENV)
+  * builds=https://console.cloud.google.com/cloud-build/builds?project=$taito_zone&query=source.repo_source.repo_name%3D%22github-${template_default_github_organization:?}-$taito_vc_repository%22 Build logs
+  * storage:ENV=https://console.cloud.google.com/storage/browser/$taito_project-$taito_env?project=$taito_resource_namespace_id Storage bucket (:ENV)
+  * logs:ENV=https://console.cloud.google.com/logs/viewer?project=$taito_zone&minLogLevel=0&expandAll=false&resource=container%2Fcluster_name%2F$kubectl_name%2Fnamespace_id%2F$taito_namespace Logs (:ENV)
+  * uptime=https://app.google.stackdriver.com/uptime?project=$taito_zone Uptime monitoring (Stackdriver)
 "
 
 # Secrets
-# TODO change secret naming convention
-export taito_secrets="
+taito_secrets="
   github-buildbot.token:read/devops
-  ${db_database_name}-db-mgr.password:random
-  ${db_database_name}-db-app.password:random
-  ${taito_project}-${taito_env}-basic-auth.auth:htpasswd-plain
+  $db_database_name-db-mgr.password:random
+  $db_database_name-db-app.password:random
+  $taito_project-$taito_env-basic-auth.auth:htpasswd-plain
 "
