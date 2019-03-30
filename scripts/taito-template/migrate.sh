ec3d513a0deab72faee7f15af9d8ceb73f5aa12f
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-03-30 16:37:49 +0200

    chore: copy latest changes from server-template

--- scripts/taito-template/migrate.sh
+++ scripts/taito-template/migrate.sh
@@ -1,28 +1,42 @@
-#!/bin/bash
+#!/bin/bash -e
 
 : "${template_project_path:?}"
 
+# Init
+./scripts/taito-template/init.sh
+
 # Move old files away from project root
-rm -rf "${template_project_path}/old_root" 2> /dev/null
-mkdir -p "${template_project_path}/old_root"
-for file in ${template_project_path}/*; do
-   if ! [ -d "$file" ]; then
-     mv -- "$file" "${template_project_path}/old_root"
-   fi
-done
-
-# Move old scripts to old_scripts
-mv "${template_project_path}/scripts" "${template_project_path}/old_scripts" \
-  2> /dev/null
-
-# Upgrade
-./scripts/taito-template/upgrade.sh
+(
+  set -e
+  cd "${template_project_path}"
+  rm -rf old_root 2> /dev/null
+  mkdir -p old_root
+  shopt -s dotglob
+  for file in *; do
+     if [[ $file != ".git" ]] && \
+        [[ $file != "old_root" ]] && \
+        [[ $file != "template-tmp" ]]
+     then
+       mv -- "$file" "old_root"
+     fi
+  done
+)
 
 # Copy all files from template root
-yes | cp -- * "${template_project_path}" 2> /dev/null
-yes | cp -- .* "${template_project_path}" 2> /dev/null
+rm -rf .git
+yes | cp -r * "${template_project_path}" 2> /dev/null || :
+yes | cp -r .* "${template_project_path}" 2> /dev/null || :
 
-# Copy files from scripts folder
-cp scripts/* "${template_project_path}/scripts" 2> /dev/null
 
+echo
+echo
+echo "--- Manual steps ---"
+echo
+echo "Mandatory:"
+echo "- Move files from 'old_root' to the new folder structure and make the project"
+echo "  work locally on Docker Compose."
+echo "- Configure project (see CONFIGURATION.md)."
+echo
+echo "Recommended additional steps:"
+echo "- Update libraries and other project dependencies."
 echo
--- scripts/taito-template/upgrade.sh
+++ scripts/taito-template/upgrade.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 : "${template_project_path:?}"
 
@@ -6,32 +6,29 @@ echo
 ./scripts/taito-template/init.sh
 
 # Copy files from template to project
-cp README.md "${template_project_path}"
-cp cloudbuild.yaml "${template_project_path}"
-mkdir -p "${template_project_path}/scripts"
+yes | cp CONFIGURATION.md "${template_project_path}"
+yes | cp DEVELOPMENT.md "${template_project_path}"
+yes | cp cloudbuild.yaml "${template_project_path}"
 
-rm -rf "${template_project_path}/scripts/helm" 2> /dev/null
-cp -r "scripts/helm" "${template_project_path}/scripts/helm" 2> /dev/null
-
-mv "${template_project_path}/scripts/terraform" "${template_project_path}/scripts/terraform_old" 2> /dev/null
-cp -r "scripts/terraform" "${template_project_path}/scripts/terraform" 2> /dev/null
-
-mkdir "${template_project_path}/wordpress_old"
-cp "${template_project_path}/wordpress/"* "${template_project_path}/wordpress_old"
-cp wordpress/* "${template_project_path}/wordpress" 2> /dev/null
+mkdir -p "${template_project_path}/scripts/helm"
+yes | cp -r "scripts/helm/requirements.yaml" \
+  "${template_project_path}/scripts/helm/requirements.yaml" 2> /dev/null
+yes | cp -r "scripts/update/*" \
+  "${template_project_path}/scripts/update" 2> /dev/null
+yes | cp -r "wordpress/Docker*" \
+  "${template_project_path}/wordpress" 2> /dev/null
 
 echo
 echo
 echo "--- Manual steps ---"
 echo
-echo "Do the following:"
-echo "- Generate links to README.md by running: taito project docs"
-echo "- Add project specific modifications back to README.md if there were any"
-echo "  (README.md was copied from the template)"
+echo "Recommended steps:"
+echo "- Update wordpress plugins and other project dependencies."
 echo
 echo "If something stops working, try the following:"
 echo "- Run 'taito --upgrade' to upgrade your taito-cli"
-echo "- Compare taito-config.sh with the old one"
-echo "- Compare package.json with the old one"
-echo "- Compare dockerfiles with the old ones"
+echo "- Compare scripts/helm*.yaml with the template"
+echo "- Compare taito-config.sh with the template"
+echo "- Compare package.json with the template"
+echo "- Compare cloudbuild.yaml with the previous version"
 echo
