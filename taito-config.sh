c1616b3975939ede51870cef48eedf2fe65c59b7
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-05-08 18:41:57 +0300

    chore: taito_vc_provider formatting

--- taito-config.sh
+++ taito-config.sh
@@ -309,7 +309,7 @@ case $taito_ci_provider in
 esac
 
 case $taito_vc_provider in
-  bitbucket)
+  bitbucket.org)
     taito_plugins="
       ${taito_plugins}
       bitbucket-ci:-local
@@ -320,7 +320,7 @@ case $taito_vc_provider in
       * project=https://$taito_vc_repository_url/addon/trello/trello-board Project management
     "
     ;;
-  github)
+  github.com)
     taito_plugins="
       ${taito_plugins}
       gcloud-ci:-local
