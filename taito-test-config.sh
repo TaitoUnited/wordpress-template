4a88cc58ab99b37416b77ade864319774233c890
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-06-14 14:28:35 +0300

    chore: move template domain to taito-domain-config.sh

---
+++ taito-test-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# shellcheck disable=SC2034
+
+##########################################################################
+# Integration and end-to-end test parameters
+# NOTE: Variables are passed to the tests without the test_TARGET_ prefix.
+##########################################################################
