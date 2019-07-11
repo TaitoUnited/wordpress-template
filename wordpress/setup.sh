2615e89f4511f6a8c6fe371fe81d62af98a3ffb2
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-07-11 10:34:31 +0300

    fix: wp-config.php

--- wordpress/setup.sh
+++ wordpress/setup.sh
@@ -3,8 +3,11 @@
 # Custom wordpress-template implementation for fetching database settings
 # from environment variables.
 # https://github.com/bitnami/bitnami-docker-wordpress/issues/128#issuecomment-389427845
-# TODO: db password should be read from disk mount
 
+# TODO: all secrets (e.g. db password) should be read from disk mount instead of env vars
+# TODO: replace also salts and keys, so that they can be kept in kubernetes secrets
+# TODO: ---> replace all of this with a wp-config.php mounted from secret
+#
 echo "setup.sh: Adding support for database environment variables"
 
 echo "setup.sh: Modifying getDatabaseProperties() function of helpers.js"
@@ -18,23 +21,21 @@ cat /template/helpers.js \
 
 echo "setup.sh: Updating wp-config-sample.php with environment variable references"
 cp /opt/bitnami/wordpress/wp-config-sample.php /opt/bitnami/wordpress/wp-config-sample.orig.php
-sed "s/define('DB_NAME'.*$/define('DB_NAME', getenv('WORDPRESS_DATABASE_NAME'));/" \
+sed "s/define( 'DB_NAME'.*$/define( 'DB_NAME', getenv('WORDPRESS_DATABASE_NAME'));/" \
   /opt/bitnami/wordpress/wp-config-sample.orig.php |
-sed "s/define('DB_USER'.*$/define('DB_USER', getenv('WORDPRESS_DATABASE_USER'));/" |
-# TODO: db password should be read from disk mount
-sed "s/define('DB_PASSWORD'.*$/define('DB_PASSWORD', getenv('WORDPRESS_DATABASE_PASSWORD'));/" |
-sed "s/define('DB_HOST'.*$/define('DB_HOST', getenv('MARIADB_HOST') . ':' . getenv('MARIADB_PORT_NUMBER'));/" \
+sed "s/define( 'DB_USER'.*$/define( 'DB_USER', getenv('WORDPRESS_DATABASE_USER'));/" |
+sed "s/define( 'DB_PASSWORD'.*$/define( 'DB_PASSWORD', getenv('WORDPRESS_DATABASE_PASSWORD'));/" |
+sed "s/define( 'DB_HOST'.*$/define( 'DB_HOST', getenv('MARIADB_HOST') . ':' . getenv('MARIADB_PORT_NUMBER'));/" \
   > /opt/bitnami/wordpress/wp-config-sample.php
 
 # TODO: duplicate code
 echo "setup.sh: Updating wp-config.php with environment variable references"
 cp /opt/bitnami/wordpress/wp-config.php /opt/bitnami/wordpress/wp-config.orig.php
-sed "s/define('DB_NAME'.*$/define('DB_NAME', getenv('WORDPRESS_DATABASE_NAME'));/" \
+sed "s/define( 'DB_NAME'.*$/define( 'DB_NAME', getenv('WORDPRESS_DATABASE_NAME'));/" \
   /opt/bitnami/wordpress/wp-config.orig.php |
-sed "s/define('DB_USER'.*$/define('DB_USER', getenv('WORDPRESS_DATABASE_USER'));/" |
-# TODO: db password should be read from disk mount
-sed "s/define('DB_PASSWORD'.*$/define('DB_PASSWORD', getenv('WORDPRESS_DATABASE_PASSWORD'));/" |
-sed "s/define('DB_HOST'.*$/define('DB_HOST', getenv('MARIADB_HOST') . ':' . getenv('MARIADB_PORT_NUMBER'));/" \
+sed "s/define( 'DB_USER'.*$/define( 'DB_USER', getenv('WORDPRESS_DATABASE_USER'));/" |
+sed "s/define( 'DB_PASSWORD'.*$/define( 'DB_PASSWORD', getenv('WORDPRESS_DATABASE_PASSWORD'));/" |
+sed "s/define( 'DB_HOST'.*$/define( 'DB_HOST', getenv('MARIADB_HOST') . ':' . getenv('MARIADB_PORT_NUMBER'));/" \
   > /opt/bitnami/wordpress/wp-config.php
 
 echo "setup.sh: DONE"
