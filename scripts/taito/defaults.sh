45129cf09d9e8b77a086f83177eb9a0e3c808f31
keskiju <jukka.keskiluopa@taitounited.fi>
2021-04-20 01:04:45 +0300

    chore: copy latest changes from full-stack-template

--- scripts/taito/defaults.sh
+++ scripts/taito/defaults.sh
@@ -3,11 +3,10 @@
 # shellcheck disable=SC2154
 
 ##########################################################################
-# Project specific labels
-#
-# Names of namespaces and resources are determined by these labels.
+# Project specific default values used for various purposes
 ##########################################################################
 
+# Labels
 taito_project=wordpress-template
 taito_project_short=wptemplate # Max 10 characters
 taito_random_name=wordpress-template
@@ -15,15 +14,15 @@ taito_company=companyname
 taito_family=
 taito_application=template
 taito_suffix=
-
-# Namespace
 taito_namespace=wordpress-template-$taito_env
 
-# Database defaults
-taito_default_db_type=mysql
-taito_default_db_shared=false # If true, taito_random_name is used as pg db name
-
-# Template
+# Template reference
 template_version=1.0.0
 template_name=WORDPRESS-TEMPLATE
 template_source_git=git@github.com:TaitoUnited
+
+# Database defaults
+# Override database defaults here, if you want to use some other than the
+# zone default database cluster.
+taito_default_db_type=mysql
+taito_default_db_shared=false # If true, taito_random_name is used as pg db name
