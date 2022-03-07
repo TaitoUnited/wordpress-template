8269462785ebf80a0ef6310652f035930e9aace7
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2022-03-07 10:56:08 +0200

    fix: paths, enable build on stag

---
+++ scripts/taito/env-stag.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+# shellcheck disable=SC2154
+
+##########################################################################
+# Staging environment settings
+##########################################################################
+
+# Allow building to stag
+ci_exec_build=true
